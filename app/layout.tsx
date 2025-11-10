4959db315505d5180208d1675e939432ab37372e
Akshay Bavaliya <akshaybavaliya007@gmail.com>
2025-11-10 14:06:21 +0530

    small fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,17 +56,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-// (function(d,t) {
-//   var BASE_URL="https://app.chatwoot.com";
-//   var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-//   g.src=BASE_URL+"/packs/js/sdk.js";
-//   g.async = true;
-//   s.parentNode.insertBefore(g,s);
-//   g.onload=function(){
-//     window.chatwootSDK.run({
-//       websiteToken: 'yiiHYFbbf4pjyM2W2XVer1hc',
-//       baseUrl: BASE_URL
-//     })
-//   }
-// })(document,"script");
