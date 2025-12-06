5366bca8287f3ef443507827baf228d51f3f1568
drianshsofttech <support@driansh.com>
2025-12-06 16:52:36 +0530

    Update page.tsx

--- app/our-products/omniconnect/chatbots/page.tsx
+++ app/our-products/omniconnect/chatbots/page.tsx
@@ -19,9 +19,12 @@ export default function ChatbotsPage() {
             satisfaction and loyalty.
           </p>
 
-          <button className="inline-flex items-center justify-center rounded-md bg-blue-600 px-6 py-3 text-sm font-semibold text-white shadow-sm hover:bg-blue-700 transition-colors">
-            Request a demo
-          </button>
+          <Link
+              href="/contact-us"
+              className="inline-flex items-center justify-center rounded-md bg-blue-600 px-6 py-3 text-sm font-semibold text-white shadow-sm hover:bg-blue-700 transition-colors"
+              >
+              Request a demo
+            </Link>
         </div>
       </section>
 
